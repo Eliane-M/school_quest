c45faa6af10c2891a4fbe394ed3d9fef55aca1f8
Eliane_M <munezeroeliane761@gmail.com>
2025-03-07 09:53:47 +0200

    Initial commit

---
+++ school_quest/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'signin_page.dart';
+import 'welcome_page.dart';
+import 'signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SheTech',
+      theme: ThemeData(
+        fontFamily: 'Outfit',
+        primaryColor: const Color(0xFF9D4EDD),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const WelcomePage(),
+        '/signin': (context) => const SignInScreen(),
+        '/signup': (context) => const SignupScreen()
+      },
+    );
+  }
+}
